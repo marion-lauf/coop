3abfd52c402118dacf8924cefab806e551e1fd56
marion-lauf <marion.lauf@gmail.com>
2017-06-06 18:55:16 +0200

    Etape 4
    Home

--- wp-content/themes/FoundationPress-master/index.php
+++ wp-content/themes/FoundationPress-master/index.php
@@ -16,37 +16,66 @@
 get_header(); ?>
 
 	<div class="row">
-		<img src="http://via.placeholder.com/1121x525">
+		<img src="http://via.placeholder.com/1440x525">
 	</div>
 
+
+
 <div class="main-wrap" role="main">
-	<article class="main-content">
-	<?php if ( have_posts() ) : ?>
 
-		<?php /* Start the Loop */ ?>
-		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
-		<?php endwhile; ?>
 
-		<?php else : ?>
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
-		<?php endif; // End have_posts() check. ?>
+	<div class="row">
+		<h1><b>RÉAMÉNAGEMENT DU QUARTIER COOP STRASBOURG</b></h1>
+		<br />
+		<div class="large-6 columns">
+			<h2>Strasbourg a lancé de vastes projets de réhabilitation de ses friches industrielles (<b>Presqu'Ile Malraux, route du Rhin, ZAC des Deux Rives</b>) en nouveaux quartiers de la ville mêlant habitations, bureaux et espaces d'innovations artistiques ou entrepreunariales.</h2>
+		</div>
 
-		<?php /* Display navigation to next/previous pages when applicable */ ?>
-		<?php
-		if ( function_exists( 'foundationpress_pagination' ) ) :
-			foundationpress_pagination();
-		elseif ( is_paged() ) :
-		?>
-			<nav id="post-nav">
-				<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
-				<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
-			</nav>
-		<?php endif; ?>
+		<div class="large-6 columns">
+			<h2>Acquis en 2015 par la ville de Strasbourg, <b>les locaux de la Coop</b> et les parcelles avoisinantes ont donné lieu à différents appels à projets. Lors d'une conférence de presse le 10 avril dernier, la ville annonce les premiers projets retenus pour transformer et occuper les locaux.</h2>
+		</div>
+	</div>
 
-	</article>
 
+	<br />
+	<div class="row">
+		<div class="large-1 columns">
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/demi-lune.png" alt="Logo" class="demi-lune"/>
+		</div>
+		<div class="large-2 text-left columns">
+			<h4><b>la demi-lune</b></h4>
+		</div>
+
+		<div class="large-1 columns">
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/virgule.png" alt="Logo" class="virgule"/>
+		</div>
+		<div class="large-2 text-left columns end">
+			<h4><b>la virgule</b></h4>
+		</div>
+	</div>
+
+	<br />
+
+	<div class="row contact">
+			<h3 align="center"><b>Vous avez un projet en tête, réalisable au quartier de la Coop ?</b></h3>
+			<button class="hollow button success" href="#">CONTACT</button>
+	</div>
+
+
+<div class="row">
+	<div class="row small-up-1 medium-up-2 large-up-3">
+	  <div class="column column-block">
+	    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/1.png" alt="Logo" class="1"/>
+	  </div>
+	  <div class="column column-block">
+	    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/2.png" alt="Logo" class="2"/>
+	  </div>
+	  <div class="column column-block">
+	    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/3.png" alt="Logo" class="3"/>
+	  </div>
+	</div>
+</div>
 
 </div>
 
